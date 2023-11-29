8f1a4c715746585b58ab2bbfee94000611784ded
SAYANTAN ROY <rivurox@tamu.edu>
2023-11-28 18:24:03 -0600

    documented function to generate data

--- R/DataGen.R
+++ R/DataGen.R
@@ -1,9 +1,21 @@
-##Generating data from latent factor model
+#' Generates data-sets from latent factor model
+#'
+#' @param n Numeric, number of data points in each repetition
+#' @param p Numeric, dimension of each data point
+#' @param k Numeric, number of factors to generate from
+#' @param rep Numeric, number of repetitions
+#'
+#' @return list containing data-sets generated from latent factor model with error variance 0.01, true variance, Lambda and its rank.
+#'
+#' @export
+#'
+#' @examples
+#' newData = generateData( n = 1000, p = 50, k = 10, rep = 5 )
 
-##library
-library(MASS)
 
 generateData = function( n, p, k, rep ){
+
+  N = n * rep
 
 # Initialize variables
 Lambda = matrix(0, p, k)
